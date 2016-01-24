6b2aaace455e184bca8ee9477c24ebb68000a993
Bella <isabella.lipari@yahoo.ca>
2016-01-24 13:55:39 -0500

    initial commit, added count.sh

---
+++ count.sh
@@ -0,0 +1,21 @@
+aiden_2015.txt
+   1 marlin
+   1 shark
+dmitri_2015.txt
+   1 cod
+   1 flying fish
+   2 marlin
+   3 shark
+   1 tuna
+   1 turtle
+matt_2014.txt
+   2 turtle
+matt_2015.txt
+   2 turtle
+seen.txt
+   1    1 cod
+   1    1 flying fish
+   1    1 tuna
+   1    1 turtle
+   1    2 marlin
+   1    3 shark
